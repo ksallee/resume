6fef209ef87695dcff5a014b29597610c6734a30
Kevin Sallee <kevin.sallee@gpltech.com>
2025-01-10 14:12:11 -0600

    fixed server file

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,15 +1,9 @@
-import { json } from '@sveltejs/kit';
-import fs from 'fs';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-export const load = async () => {
-    const filePath = join(__dirname, '../../static/data/resume.json');
-    const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-    console.log("data", data.skills);
+export const load = async ({fetch}) => {
+    const response = await fetch('/data/resume.json');
+    if (!response.ok) {
+        throw new Error('Failed to fetch resume.json');
+    }
+    const data = await response.json();
     return {
         profile: data
     };
